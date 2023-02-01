db06bd8f9e9094dac579b308265bd169e792266f
Marius Kittler <mkittler@suse.de>
2023-02-01 14:39:00 +0100

    Add GitHub action triggering actual openQA jobs related to current PR
    * See https://progress.opensuse.org/issues/125723 (and https://progress.opensuse.org/issues/124502)
    * See https://hackweek.opensuse.org/22/projects/trigger-actual-openqa-tests-in-pull-requests for details

--- tests/boot.pm
+++ tests/boot.pm
@@ -9,6 +9,9 @@ sub run {
     # wait for bootloader to appear
     assert_screen 'bootloader';
 
+    # conclude test early if there's no boot media
+    return undef if match_has_tag 'no-boot-media';
+
     # press enter to boot right away
     send_key 'ret';
 
